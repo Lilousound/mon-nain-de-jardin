382eff65f36ddec2d1a0569777912ccee19c2511
Aurelie Raout <aurelieraout@gmail.com>
2026-02-12 16:35:03 +0100

    fixed some ts issues

--- app/page.tsx
+++ app/page.tsx
@@ -7,20 +7,23 @@ import ShoppingList from './components/ShoppingList'
 import Footer from './components/Footer'
 import './styles/Layout.css'
 import { useState, useEffect } from 'react'
+import { CartProps } from './types'
 
 export default function Home() {
   // Initialise le state avec une valeur par défaut (côté serveur)
-  const [cart, updateCart] = useState([]);
+  const [cart, updateCart] = useState<CartProps['cart']>([]);
 
   // Charge les données depuis localStorage après le rendu initial (côté client)
   useEffect(() => {
     if (typeof window !== 'undefined') {
       const savedCart = localStorage.getItem('cart');
       if (savedCart) {
-        updateCart(JSON.parse(savedCart));
+        setTimeout(() => {
+          updateCart(JSON.parse(savedCart));
+        }, 0);
       }
     }
-  }, []); // Le tableau vide [] signifie que ce useEffect ne s'exécute qu'une fois, après le rendu initial
+}, []);
 
   // Met à jour localStorage quand le panier change
   useEffect(() => {
